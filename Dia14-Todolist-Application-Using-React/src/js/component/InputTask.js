58620b19ef257454200538b2731c6d33cfd17412
Ehiber <ehiber5@hotmail.com>
2019-11-21 18:55:17 -0400

    agregando bp e iniciando el proyecto

---
+++ Dia14-Todolist-Application-Using-React/src/js/component/InputTask.js
@@ -0,0 +1,13 @@
+import React, { ReactFragment } from "react";
+
+const InputTask = props => {
+	return (
+		<React.Fragment>
+			<div>
+				<h1>hola soy el mapa</h1>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default InputTask;
---
+++ Dia14-Todolist-Application-Using-React/src/js/component/home.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+import InputTask from "./InputTask.js";
+//create your first component
+export function Home() {
+	return (
+		<div className="text-center mt-5">
+			<h1>Hello Rigo!</h1>
+			<p>
+				<img src={rigoImage} />
+			</p>
+			<a href="#" className="btn btn-success">
+				If you see this green button... bootstrap is working
+			</a>
+			<p>
+				Made by{" "}
+				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				love!
+			</p>
+
+			<InputTask />
+		</div>
+	);
+}
